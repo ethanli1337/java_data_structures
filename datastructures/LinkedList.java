a26507582648f14b5351527509597cbdd622d8fd
Ethan Li <ethanli2756@gmail.com>
2026-04-02 23:07:19 -0500

    Implement LinkedList methods and add Hello class for testing

--- datastructures/LinkedList.java
+++ datastructures/LinkedList.java
@@ -12,14 +12,23 @@ public class LinkedList {
         public Node(int value) {
             this.value=value;
 
+        }
     }
-}
+    
     public LinkedList(int value) {
         Node newNode = new Node(value);
         this.head = newNode;
         this.tail = newNode;
         this.length = 1;
     }   
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value); 
+            temp = temp.next;
+        }
+    }
 
     public void append(int value) {
 
@@ -32,5 +41,17 @@ public class LinkedList {
     public boolean insert(int index, int value) {
 
         return true;
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
     }
 }
--- datastructures/Main.java
+++ datastructures/Main.java
@@ -4,5 +4,10 @@ public class Main {
     
     public static void main(String[] args) {
         LinkedList linked = new LinkedList(2);
+        linked.getHead();
+        linked.getTail();
+        linked.getLength();
+
+        linked.printList();
     }
 }
